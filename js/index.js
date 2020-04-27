dc308c787f65b112029bd56e0f07651c1d936102
Saira Rehman <saira@Sairas-MacBook-Pro.local>
2020-04-26 21:52:26 -0400

    variable element assigned to get the elements

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
 
 // Get all the headers from each section
 var headers = document.querySelectorAll("h2");
+
+// Get the element from each secation 
+var elements = document.getElementById("scrolsection");
