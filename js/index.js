248e42a4cc9f0d26b728c8a39e38f0c398d2640e
Saira Rehman <saira@Sairas-MacBook-Pro.local>
2020-04-26 22:22:04 -0400

    Function to add or remove sticky class implemented

--- js/index.js
+++ js/index.js
@@ -3,4 +3,17 @@
 var headers = document.querySelectorAll("h2");
 
 // Get the element from each secation 
-var elements = document.getElementById("scrolsection");
+var element = document.getElementById("scrollsection");
+
+function myFunction() {
+	headers.forEach(header => {
+        const sticky = header.offsetTop;
+           if (window.pageYOffset > sticky){
+            header.classList.add("sticky");
+		} else {
+			header.classList.remove("sticky");
+		}
+	})
+}
+window.addEventListener('scroll', myFunction)
+
