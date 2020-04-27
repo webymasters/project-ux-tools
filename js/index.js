389b11e7a7bc9f738c6cbf5cf995fe4cf670b2fd
Saira Rehman <saira@Sairas-MacBook-Pro.local>
2020-04-26 23:30:11 -0400

    added comments to the myFunction

--- js/index.js
+++ js/index.js
@@ -5,9 +5,11 @@ var headers = document.querySelectorAll("h2");
 // Get the element from each secation 
 var element = document.getElementById("scrollsection");
 
+// For each heading 
 function myFunction() {
 	headers.forEach(header => {
-        const sticky = header.offsetTop;
+		const sticky = header.offsetTop;
+// Check if Scroll position > sticky, if yes add fixed heading otherwise remove fixed heading
            if (window.pageYOffset > sticky){
             header.classList.add("sticky");
 		} else {
